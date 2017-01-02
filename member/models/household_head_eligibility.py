18dbb03bd8b35e17c814c7c4d4b161edc1f8a333
erikvw <ew2789@gmail.com>
2017-01-03 00:00:44 +0200

    move validation to common clean

--- member/models/household_head_eligibility.py
+++ member/models/household_head_eligibility.py
@@ -1,5 +1,3 @@
-from django.core.exceptions import ValidationError
-
 from edc_base.model.models import HistoricalRecords, BaseUuidModel
 
 from ..managers import MemberEntryManager
@@ -21,20 +19,6 @@ class HouseholdHeadEligibility(RepresentativeEligibilityMixin, HouseholdMemberMo
     def natural_key(self):
         return self.household_member.natural_key()
     natural_key.dependencies = ['member.householdmember']
-
-    def save(self, *args, **kwargs):
-        # self.matches_household_member_values(self.household_member)
-        super(HouseholdHeadEligibility, self).save(*args, **kwargs)
-
-    def matches_household_member_values(self, household_member, exception_cls=None):
-        """Compares shared values on household_member form and
-        returns True if all match."""
-        error_msg = None
-        exception_cls = exception_cls or ValidationError
-        if not household_member.age_in_years >= 18:
-            raise exception_cls('Household member must be over 18 years of age. '
-                                'Got {0}.'.format(household_member.age_in_years))
-        return error_msg
 
     class Meta:
         app_label = 'member'
