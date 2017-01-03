e2eec345fa8ca32d89e18d6b1b331d2a5bd43f3b
erikvw <ew2789@gmail.com>
2017-01-03 22:22:28 +0200

    use reverse relations, add attr for final status pending

--- member/participation_status.py
+++ member/participation_status.py
@@ -1,30 +1,36 @@
 from edc_constants.constants import CONSENTED, REFUSED
 
-from .models import DeceasedMember, RefusedMember, AbsentMember, UndecidedMember, HtcMember
 from .constants import AVAILABLE, DECEASED, HTC_ELIGIBLE, ABSENT, UNDECIDED
+from .models import DeceasedMember, RefusedMember, HtcMember
 
 
 class ParticipationStatus:
     def __init__(self, household_member):
         if household_member.is_consented:
             participation_status = CONSENTED
+            reports = [(CONSENTED, None)]
         else:
             participation_status = None
-            for model, label in [(DeceasedMember, DECEASED), (HtcMember, HTC_ELIGIBLE), (RefusedMember, REFUSED)]:
+            for model, label in [
+                    (DeceasedMember, DECEASED), (HtcMember, HTC_ELIGIBLE), (RefusedMember, REFUSED)]:
                 try:
                     model.objects.get(household_member=household_member)
                     participation_status = label
                 except model.DoesNotExist:
                     pass
             if not participation_status:
-                absent_members = AbsentMember.objects.filter(
-                    household_member=household_member).order_by('report_date')
-                reports = [(ABSENT, absent_member.report_date) for absent_member in absent_members]
-                undecided_members = UndecidedMember.objects.filter(
-                    household_member=household_member).order_by('report_date')
+                absent_members = household_member.absentmember_set.all().order_by('report_date')
+                reports = [(ABSENT, absent_member.report_date, absent_member)
+                           for absent_member in absent_members]
+                undecided_members = household_member.undecidedmember_set.all().order_by('report_date')
                 reports.extend(
-                    [(UNDECIDED, undecided_member.report_date) for undecided_member in undecided_members])
+                    [(UNDECIDED, undecided_member.report_date, undecided_member)
+                     for undecided_member in undecided_members])
                 if reports:
                     reports.sort(key=lambda x: x[1])
                     participation_status = reports[-1:][0][0]
         self.participation_status = participation_status or AVAILABLE
+        if self.participation_status in [ABSENT, UNDECIDED, AVAILABLE, HTC_ELIGIBLE]:
+            self.final_status_pending = True
+        else:
+            self.final_status_pending = False
