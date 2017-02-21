f394e7915e19f417f54175b7deeae3ba0d702a89
Coulson <ckgathi@bhp.org.bw>
2017-02-21 20:22:46 +0200

    list view filter by sectioned plots

--- member/views/listboard/listboard.py
+++ member/views/listboard/listboard.py
@@ -6,6 +6,7 @@ from django.db.models import Q
 from django.utils.decorators import method_decorator
 
 from edc_dashboard.forms import SearchForm as BaseSearchForm
+from edc_map.models import InnerContainer
 
 from .base_listboard import BaseListboardView
 
@@ -33,6 +34,15 @@ class ListboardView(BaseListboardView):
 
     def get_queryset_filter_options(self, request, *args, **kwargs):
         options = super().get_queryset_filter_options(request, *args, **kwargs)
+        plot_identifier_list = []
+        try:
+            plot_identifier_list = InnerContainer.objects.get(
+                username=request.user.username).identifier_labels
+        except InnerContainer.DoesNotExist:
+            plot_identifier_list = []
+        if plot_identifier_list:
+            options.update(
+                {'household_structure__household__plot__plot_identifier__in': plot_identifier_list})
         if kwargs.get('plot_identifier'):
             options.update({
                 'household_structure__household__plot__plot_identifier':
