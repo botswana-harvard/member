bf83ef8c46720435111e194b390090adebd35118
Tshepiso Setsiba <tsetsiba@bhp.org.bw>
2017-02-21 13:36:42 +0200

    fix replace subject_identifier_as_pk with internal_identifier

--- member/managers.py
+++ member/managers.py
@@ -4,12 +4,12 @@ from django.db import models
 class HouseholdMemberManager(models.Manager):
 
     def get_by_natural_key(self,
-                           subject_identifier_as_pk,
+                           internal_identifier,
                            survey_schedule,
                            household_identifier,
                            plot_identifier):
         return self.get(
-            subject_identifier_as_pk=subject_identifier_as_pk,
+            internal_identifier=internal_identifier,
             household_structure__survey_schedule=survey_schedule,
             household_identifier=household_identifier,
             household_structure__household__plot__plot_identifier=plot_identifier
@@ -19,15 +19,15 @@ class HouseholdMemberManager(models.Manager):
 class MemberEntryManager(models.Manager):
 
     def get_by_natural_key(self, report_datetime,
-                           subject_identifier_as_pk,
+                           internal_identifier,
                            survey_schedule,
                            household_identifier,
                            plot_identifier):
 
         options = {
             'report_datetime': report_datetime,
-            'household_member__subject_identifier_as_pk':
-            subject_identifier_as_pk,
+            'household_member__internal_identifier':
+            internal_identifier,
             'household_member__household_structure__survey_schedule':
             survey_schedule,
             'household_member__household_structure__household__household_identifier':
