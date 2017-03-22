00c56f0d2f47f5d1c67d5f159f606b18e0f49084
erikvw <ew2789@gmail.com>
2017-03-23 01:01:51 +0200

    add space to listboard, move plot and household filter/exclude in view to mixin

--- member/views/listboard/anonymous.py
+++ member/views/listboard/anonymous.py
@@ -16,12 +16,3 @@ class AnonymousListboardView(BaseListboardView):
     @method_decorator(login_required)
     def dispatch(self, *args, **kwargs):
         return super().dispatch(*args, **kwargs)
-
-    def get_queryset_filter_options(self, request, *args, **kwargs):
-        options = super().get_queryset_filter_options(
-            request, *args, **kwargs)
-        plot_identifier = django_apps.get_app_config(
-            'plot').anonymous_plot_identifier
-        options.update(
-            {'household_structure__household__plot__plot_identifier': plot_identifier})
-        return options
