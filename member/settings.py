14c46a29e824ecc88dc63401a5790edc41ba15d5
Itumeleng Mosweu <imosweu@nkange.bhp.org.bw>
2017-03-24 17:48:24 +0200

    add survey group name for tests

--- member/settings.py
+++ member/settings.py
@@ -183,3 +183,5 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'member', 'media')
 CURRENT_MAP_AREA = 'test_community'
 DEVICE_ID = '99'
+KEY_PATH = '/Users/imosweu/source/member/crypto_fields'
+SURVEY_GROUP_NAME = 'test_survey'
