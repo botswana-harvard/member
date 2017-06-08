0e87ca09bed2ebf9108ca9ae7624655ec6810f07
Tshepiso Setsiba <tsetsiba@bhp.org.bw>
2017-06-08 17:48:13 +0200

    sync models

--- member/sync_models.py
+++ member/sync_models.py
@@ -1,26 +1,13 @@
+from django.apps import apps as django_apps
+
+from edc_base.model_mixins import ListModelMixin
 from edc_sync.site_sync_models import site_sync_models
 from edc_sync.sync_model import SyncModel
 
-sync_models = [
-    'member.absentmember',
-    'member.deceasedmember',
-    'member.enrollmentchecklist',
-    'member.enrollmentloss',
-    'member.householdheadeligibility',
-    'member.householdinfo',
-    'member.householdmember',
-    'member.householdworklist',
-    'member.htcmemberhistory',
-    'member.htcmember',
-    'member.listmodels',
-    'member.memberappointment',
-    'member.movedmember',
-    'member.refusedmemberhistory',
-    'member.refusedmember',
-    'member.representativeeligibility',
-    'member.undecidedmemberhistory',
-    'member.undecidedmember'
-]
-
+sync_models = []
+app = django_apps.get_app_config('member')
+for model in app.get_models():
+    if not issubclass(model, ListModelMixin):
+        sync_models.append(model._meta.label_lower)
 
 site_sync_models.register(sync_models, SyncModel)
