2bc2060f23f240163d61b01cf56d08b848727dbb
Itumeleng Mosweu <imosweuk@gmail.com>
2018-01-22 12:00:59 +0200

    update delete_wrong_members management command

--- member/management/commands/delete_wrong_members.py
+++ member/management/commands/delete_wrong_members.py
@@ -1,17 +1,31 @@
 from datetime import datetime
 from django.core.management.base import BaseCommand
 
+from edc_map.models import InnerContainer
 from edc_sync.models import OutgoingTransaction
 from ...models import (
     HouseholdMember, HouseholdHeadEligibility, EnrollmentChecklist,
-    AbsentMember, DeceasedMember, HtcMember, RefusedMember, UndecidedMember)
+    AbsentMember, DeceasedMember, HtcMember, MovedMember, RefusedMember, UndecidedMember)
 from bcpp_subject.models import Appointment, SubjectVisit, SubjectConsent
 from household.exceptions import HouseholdLogRequired
+from django.core.exceptions import ValidationError
+from django.conf import settings
 
 
-def delete_household_members(map_area=None, survey_schedule=None, consent_version=None):
+def delete_household_members(
+        map_area=None, survey_schedule=None, consent_version=None):
+    plot_identifiers = None
+    try:
+        inner_container = InnerContainer.objects.get(
+            device_id=settings.DEVICE_ID, map_area=map_area)
+    except InnerContainer.DoesNotExist:
+        raise ValidationError("There are no plots sectioned for this machine.")
+    else:
+        plot_identifiers = inner_container.identifier_labels
+
     household_members = HouseholdMember.objects.filter(
         household_structure__household__plot__map_area=map_area,
+        household_structure__household__plot__plot_identifier__in=plot_identifiers,
         survey_schedule=survey_schedule, cloned=True)
     count = 0
     not_household_members = []
@@ -22,7 +36,12 @@ def delete_household_members(map_area=None, survey_schedule=None, consent_versio
                 version=consent_version, household_member=household_member)
             consented += 1
         except SubjectConsent.DoesNotExist:
-            not_household_members.append(household_member)
+            try:
+                SubjectConsent.objects.get(
+                    version=consent_version, subject_identifier=household_member.subject_identifier)
+                consented += 1
+            except SubjectConsent.DoesNotExist:
+                not_household_members.append(household_member)
     print(f'consented: {consented}')
     members_to_delete = len(not_household_members)
     for household_member in not_household_members:
@@ -35,6 +54,8 @@ def delete_household_members(map_area=None, survey_schedule=None, consent_versio
             print(
                 f'Failed because of HouseholdLogRequired for {household_member}, {household_member.subject_identifier}')
         DeceasedMember.objects.filter(
+            household_member=household_member).delete()
+        MovedMember.objects.filter(
             household_member=household_member).delete()
         HtcMember.objects.filter(
             household_member=household_member).delete()
